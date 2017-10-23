d7bc3c1f60f7bf552fbeefbeec91cef6631ea16f
moniquois <32560068+moniquois@users.noreply.github.com>
2017-10-23 15:32:50 -0400

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -1,72 +1,56 @@
-#include "Player.h"
+#include "Player.hpp"
 #include <iostream>
 using namespace std;
 Player::Player()
 {
-	player_id = 0;
-	//std::cout << get_player_id() + " Player object created." << std::endl;
-
+    player_id = 0;
+    theirdice = new Dice();
+    theirhand = new Hand();
+    //std::cout << get_player_id() + " Player object created." << std::endl;
+    
 }
 
 
 Player::Player(int id) {
     using namespace std;
-	cout <<" Player " << id << " created." << endl;
-	player_id = id;
-	number_of_countries = 0;
-
+    cout <<" Player " << id << " created." << endl;
+    player_id = id;
+    number_of_countries = 0;
+    
 }
 
-   void Player::reinforce() {
+void Player::reinforce() {
     cout<<"You are in the reinforcing stage."<<endl;
-    }
+}
 
-     void Player:: attack() {
+void Player:: attack() {
     cout<<"You are in the attack stage."<<endl;
-    }
+}
 
-     void Player:: fortify() {
+void Player:: fortify() {
     cout<<"You are in the fortifying stage."<<endl;
-    }
+}
 
-   void Player::playerTurn() {
+void Player::playerTurn() {
     reinforce();
     attack();
     fortify();
-    }
+}
 
 int Player::get_player_id() {
-	return player_id;
+    return player_id;
 }
 
-
-    void Player::addCountry(Country* c){
-    countries.push_back(c);
+void Player::addCountry(Country* c){
+    theircountries.push_back(c);
+    number_of_countries ++;
 }
-
-
-void Player::removeCountry(Country* country) {
-	//An arbitrary value to be used to indicate whether the country was found in the player's countries_owned vector.
-	int index = -1;
-	int countries_owned = countries.size();
-	for(int i=0; i<countries_owned; i++) {
-		if(countries.at(i) == country) {
-			index = i;
-		}
-	}
-		if(index == -1) {
-		std::cout << "You do not own that country" << std::endl;
-	}
-	//remove country
-	else {
-		countries.erase(countries.begin()+index);
-		std::cout << "Country was erased." << std::endl;
-
-	}
+void Player::setDeck(deck *d){
+    mainDeck = d;
 }
-
-
-//Country* Player::get_country(int index) {
-	//return countries_owned.at(index);
-
-
+void Player::setDice(Dice *d){
+    theirdice = d;
+}
+void Player::setHand(Hand *h){
+    theirhand = h;
+}
